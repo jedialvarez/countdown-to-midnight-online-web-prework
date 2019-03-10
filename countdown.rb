c88f8bc9a2ca8889e637f6f66cd3b7734ed10aac
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-10 21:45:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,10 +10,10 @@ def countdown(number)
 end
 
 def  countdown_with_sleep(number)
-  while number  > 0 do
+  while number  > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(1)
+    sleep(5)
     return "HAPPY NEW YEAR!"
 
 end
